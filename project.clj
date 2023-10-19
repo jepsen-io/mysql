4c239cb5c66a7f1a55fa02ce4c9f43b7a70e9d0b
Aphyr <aphyr@aphyr.com>
2023-10-19 14:54:21 -0500

    Version 0.0.1

--- project.clj
+++ project.clj
@@ -1,13 +1,12 @@
-(defproject jepsen.mysql "0.0.1-SNAPSHOT"
+(defproject jepsen.mysql "0.0.1"
   :description "Tests for MySQL with read replicas"
   :url "https://github.com/jepsen-io/jepsen"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [cheshire "5.11.0"]
-                 [elle "0.1.8-SNAPSHOT"]
-                 [jepsen "0.3.4-SNAPSHOT"]
-                 [com.github.seancorfield/next.jdbc "1.3.883"]
+                 [cheshire "5.12.0"]
+                 [jepsen "0.3.4"]
+                 [com.github.seancorfield/next.jdbc "1.3.894"]
                  [com.mysql/mysql-connector-j "8.1.0"]
                  [org.mariadb.jdbc/mariadb-java-client "3.2.0"]]
   :main jepsen.mysql.cli
