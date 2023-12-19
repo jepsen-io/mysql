4a25155ac314d556f751d69fcbc28766a8933447
Kyle Kingsbury <aphyr@aphyr.com>
2023-12-19 09:54:25 -0600

    Bump to latest jepsen/elle

--- project.clj
+++ project.clj
@@ -5,8 +5,7 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [cheshire "5.12.0"]
-                 [jepsen "0.3.4"]
-                 [elle "0.1.9"]
+                 [jepsen "0.3.5-SNAPSHOT"]
                  [com.github.seancorfield/next.jdbc "1.3.894"]
                  [com.mysql/mysql-connector-j "8.1.0"]
                  [org.mariadb.jdbc/mariadb-java-client "3.2.0"]]
