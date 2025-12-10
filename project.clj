45c75c2eb41d8eaf78432a6ee68f78f922fa55f6
Kyle Kingsbury <aphyr@aphyr.com>
2025-12-10 17:42:55 -0600

    Dependency bumps
    Clojure 1.12.4
    Cheshire 6.1.0
    Jepsen 0.3.10
    http-kit 2.8.1
    next.jdbc 1.3.1070
    mysql-connector-j 9.5.0
    maridab-java-client 3.5.6

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "https://github.com/jepsen-io/jepsen"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [cheshire "5.12.0"]
-                 [jepsen "0.3.5"]
-                 [http-kit "2.7.0"]
-                 [com.github.seancorfield/next.jdbc "1.3.894"]
-                 [com.mysql/mysql-connector-j "8.1.0"]
-                 [org.mariadb.jdbc/mariadb-java-client "3.2.0"]]
+  :dependencies [[org.clojure/clojure "1.12.4"]
+                 [cheshire "6.1.0"]
+                 [jepsen "0.3.10"]
+                 [http-kit "2.8.1"]
+                 [com.github.seancorfield/next.jdbc "1.3.1070"]
+                 [com.mysql/mysql-connector-j "9.5.0"]
+                 [org.mariadb.jdbc/mariadb-java-client "3.5.6"]]
   :main jepsen.mysql.cli
   :repl-options {:init-ns jepsen.mysql.cli}
   :jvm-opts ["-Djava.awt.headless=true"
